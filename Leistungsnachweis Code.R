6477db155388295d0ee2d6a3f5d6d7332f937631
Michael Murawski <44762912+MichaelMurawski@users.noreply.github.com>
2019-01-01 08:47:40 +0100

    Update Leistungsnachweis Code.R

--- Leistungsnachweis Code.R
+++ Leistungsnachweis Code.R
@@ -1,24 +1,24 @@
-###Start der Hausarbeit
+###Assignment
 
-#von Michael Murawski 6356208
+#by Michael Murawski
 
-############################Aufgabe 1
+############################ Task 1
 
-setwd("C:/Users/Michael/Desktop/Uni R")
+setwd("C:/Users/Michael/Desktop/Uni R") #please adjust
 getwd
 read.table("leistungstest.dat")
 
 MyData<-read.table("leistungstest.dat",header=TRUE)
 
-MyData[order(MyData$schule,MyData$klasse),] #Sortieren nach Schulen und Klassen
+MyData[order(MyData$schule,MyData$klasse),] #Sort by schools and classes
 
 
 #levels(MyData$geschl)<-c("male","female")
 #levels(MyData$geschl)
-################# Wenn man Geschlecht doch nicht als Zahlen haben möchte
+################# Transform prior gender (0,1) with male,female
 
 
-MyData$geschl <- factor(MyData$geschl) #Umwandlung in Faktor
+MyData$geschl <- factor(MyData$geschl) #Transformation into factor
 str(MyData$geschl)
 MyData$geschl
 
@@ -27,78 +27,81 @@ install.packages("psych")
 library(psych)
 
 summary(MyData)
-#Motivation bei 95 Schülern nicht bekannt
+#Motivation data is missing for 95 students
 
 
-#############################Aufgabe 2
+############################# Task 2
 MyTable<-table(MyData$schule)
 MyTable
-which(MyTable==max(MyTable)) #Möglichkeit 1
-MyTable==max(MyTable)        #Möglichkeit 2
+which(MyTable==max(MyTable)) #Opportunity  1
+MyTable==max(MyTable)        #Opportunity  2
        
-#Schule 3 hat die meisten Teilnehmer
+#Note: school 3 has the most participants
 round(prop.table(MyTable),3)
-#Teilnehmerprozentsätze der einzelnen Schulen
+# participation in percent for each school 
 
 str(MyData$schule)
-#Schule ist nominales Skalenniveau mit 5 Ausprägungen ohne Rangfolge oder sinnvoll-interpretierbare Abstände
+#Note: school is measured with a nominal scale, has 5 outcomes, without any ranking or order and no meaningful interval interpretation 
+
 
 summary(MyData)
 
 
-############################## Aufgabe 3
+############################## Task 3
 
 
 ftable(geschl~klasse,data=MyData)
-#Tabelle mit Darstellung der Geschlechter und Klassenstufen, Absolute Häufigkeiten
+
+#Table showing the sexes and class levels in absolute frequency
 prop.table(ftable(geschl~klasse,data=MyData))
-#Relative Häufigkeiten
+
+#in relative frecuency
 
 counts <- table(MyData$geschl,MyData$klasse)
 counts
 
-############################ Absolute Häufigkeiten
+############################ absolute frequency
 
-barplot(counts, main="Verteilung der Jungen und Mädchen in der Mittelstufe",
+barplot(counts, main="Verteilung der Jungen und MÃ¤dchen in der Mittelstufe",
         xlab="Verteilung nach Geschlecht und Klassen",ylim = c(0,250), col=c("darkblue","red"),
         legend = rownames(counts), beside=TRUE)
 
-############################ Relative Häufigkeiten
+############################ relative frequency 
 
 
 counts<-table(MyData$geschl,MyData$klasse)
 prop.table(counts)
-barplot(prop.table(counts), main="Verteilung der Jungen und Mädchen in der Mittelstufe",
+barplot(prop.table(counts), main="Verteilung der Jungen und MÃ¤dchen in der Mittelstufe",
         xlab="Verteilung nach Geschlecht und Klassen (7,8,9)",ylim = c(0,0.2), col=c("blue","green"),
         legend = rownames(counts), beside=TRUE)
 
-############################ Aufgabe 4
+############################ Task 4
 
 mean(MyData$deutsch)
 median(MyData$deutsch)
-#Median und arith. Mittel aller Schulen in Deutsch
+#Median and arith. mean of all schools in German
 
 describeBy(Deutsch,Schule)
 
-########################## Alle Schulen in einem Boxplot
+########################## Boxplot diagrams for every school
 boxplot(Deutsch ~ Schule,horizontal=TRUE,las=1,ylim=c(0,50),col=rainbow(5),xlab="Punktergebnis des Deutschtests",ylab="Schule")
-########################## Schule 1
+########################## School 1
 Var1<-subset(MyData$deutsch,MyData$schule==1)
 boxplot(Var1,horizontal = T,col=rainbow(1),ylim=c(0,50))
 abline(v=mean(Var1),col='tomato')
-########################## Schule 2
+########################## School 2
 Var2<-subset(MyData$deutsch,MyData$schule==2)
 boxplot(Var2,horizontal = T,col="darkblue",ylim=c(0,50))
 abline(v=mean(Var2),col='tomato')
-########################## Schule 3
+########################## School 3
 Var3<-subset(MyData$deutsch,MyData$schule==3)
 boxplot(Var3,horizontal = T,col="blue",ylim=c(0,50))
 abline(v=mean(Var3),col='tomato')
-########################## Schule 4
+########################## School 4
 Var4<-subset(MyData$deutsch,MyData$schule==4)
 boxplot(Var4,horizontal = T,col="green",ylim=c(0,50))
 abline(v=mean(Var4),col='tomato')
-########################## Schule 5
+########################## School 5
 Var5<-subset(MyData$deutsch,MyData$schule==5)
 boxplot(Var5,horizontal = T,col="blue",ylim=c(0,50))
 abline(v=mean(Var5),col='tomato')
@@ -111,35 +114,34 @@ abline(v=mean(Var5),col='tomato')
 subset(MyData,schule==3 & Deutsch==47)
 MyData$deutsch[MyData$deutsch==47] <- NA 
 summary(MyData$deutsch)
-#1.Schüler identifizieren 2.Deutsch-Note entfernen 3. Überprüfen,ob wirklich nur der eine Wert entfernt wurde
-#altes arithm. Mittel=19,79 und alter Median=20
+#1.identify the students you are looking for 
+#2.remove german grade 
+#3. check if really only one value has been removed
+#Old arithmetic. mean=19,79 and old median=20
 
 mean(MyData$deutsch,na.rm=TRUE)
 median(MyData$deutsch,na.rm=TRUE)
 
 
-#Der Median ist robust gegenüber Außreißern .... arithm. Mittel nicht daher verändert sich das arithm. Mittel auch minimal nach unten - Konstruktionsgemäß -
+#The median is robust against outliers ....The arithmetic mean is not. Therefore the arithmetic mean changes minimally downwards
 
-#
-
-
-##################################### Aufgabe 5
+##################################### Task 5
 MyData<- cbind ( MyData , "Schull"= MyData$deutsch + MyData$mathe + MyData$englisch)
 describe(MyData$Schull,na.rm=TRUE)
-#ODER einzeln zB.
+#or for each single school with:
 #max(MyData$Schull,na.rm=TRUE)
 #min(MyData$Schull,na.rm=TRUE)
-hist(MyData$Schull,main="Schulleistung der Schüler",xlab="Anzahl der erreichten Punkte",ylab="Anzahl der Schüler",xlim=c(10,140),ylim=c(0,400),border="blue", col=terrain.colors(15),las=1)
 
-##################################### Aufgabe 6
-#Spearman-Korrelation anwenden,da erreichte Mathepunkte (metrisch) mit Motivation (ordinal) zu vergleichen ist. (Spearman oder Kendall Methode)
+hist(MyData$Schull,main="Schulleistung der SchÃ¼ler",xlab="Anzahl der erreichten Punkte",ylab="Anzahl der SchÃ¼ler",xlim=c(10,140),ylim=c(0,400),border="blue", col=terrain.colors(15),las=1)
+
+##################################### Task 6
+#Use Spearman correlation, since achieved math points (metric) shall be compared with motivation (ordinal). (Spearman or Kendall method)
 cor(MyData$motivation,MyData$mathe,method="spearman",use ="complete.obs")
 cor(MyData$motivation,MyData$mathe,method="spearman",use ="pairwise.complete.obs")
-#Korrelation zwischen der motivation der Schüler und ihren matheergebnissen ist nur leicht positiv linear, vermutlich nur schwacher zusammenhang
+#correlation between students' motivation and their maths results is only slightly positive linear, probably only weak relationship
 plot(MyData$mathe~MyData$motivation, ylab='Mathenote', xlab='Motivation') 
 
-##################################### Aufgabe 7
-read.csv("C:/Users/Michael/Desktop/Uni R", header=T)
-?read.csv
-#Es hat nur einmal funktioniert und seitdem kommt eine Meldung mit "Permission denied" und ich verstehe nicht wieso? Ich habe seitdem nichts geändert...
-
+##################################### Task 7
+my_data3 <- read_excel(file.choose())
+my_data3 <- read_excel("", sheet = "data")
+#both methods usable to integrate 
